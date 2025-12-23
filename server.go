e4cc009a8b599b3ba0cb4faef1b26e8544259569
Cnypu <iqtor519@gmail.com>
2025-12-23 17:56:04 +0300

    Create server.go

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	data := Response{
+		Message: "кд чд",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Сервер запущен на 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
